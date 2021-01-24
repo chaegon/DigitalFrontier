75cfd90f16dfed16b082a22de84fbd88bc2a86d6
SANGHYO LEE <adsff123@gmail.com>
2021-01-24 17:16:18 +0900

    main -> server_main 파일명 변경 get_main_info_chart() 수정

--- 소스/server/load_db.py
+++ 소스/server/load_db.py
@@ -41,6 +41,8 @@ def index_load_initial():
     kpi200_df.to_sql('DAILY_KPI200_INDEX', conn)
 
 def index_load_daily():
+    # 2. 추가적재
+    #  - 마지막 적재 일로 부터 오늘날짜까지 적
     conn = sqlite3.connect("DIGITALFRONTIER.db", isolation_level=None)
     cur = conn.cursor()
     max_date_query = cur.execute("SELECT MAX(Date) From DAILY_KOSPI_INDEX")
@@ -68,8 +70,38 @@ def index_load_daily():
         kosdaq_df.to_sql('DAILY_KOSDAQ_INDEX', conn, if_exists='append')
         kpi200_df.to_sql('DAILY_KPI200_INDEX', conn, if_exists='append')
 
-# 초기 적재 수행
-index_load_initial()
 
-# 일별 수행
-index_load_daily()
+def stocks_load_all():
+    # 3. 코스피, 코스닥 전체 종목코드 적재
+
+    # 한국거래소 홈페이지 크롤링이 막혔기 때문에 csv 파일 업로드로 대체 (2021-01-24)
+    # csv 다운로드 : http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020501
+    # 3-1. 전체 코스피 종목 정보 csv파일 load
+    kospi_all_stocks_df = pd.read_csv("kospi_data_20210124.csv", dtype={'종목코드': 'str'}, encoding='euc-kr')
+    kospi_code_data = kospi_all_stocks_df[['종목코드', '종목명']]
+    kospi_code_data.columns = ['code', 'name']
+    kospi_code_data.insert(0, "type", "KOSPI", True)
+
+    # 3-2. 전체 코스닥 종목 정보 csv파일 load
+    kosdaq_all_stocks_df = pd.read_csv("kosdaq_data_20210124.csv", dtype={'종목코드': 'str'}, encoding='CP949')
+    kosdaq_code_data = kosdaq_all_stocks_df[['종목코드', '종목명']]
+    kosdaq_code_data.columns = ['code', 'name']
+    kosdaq_code_data.insert(0, "type", "KOSDAQ", True)
+
+    code_data = pd.concat([kospi_code_data, kosdaq_code_data], ignore_index=True)
+
+    # DB 생성 (오토 커밋)
+    conn = sqlite3.connect("DIGITALFRONTIER.db", isolation_level=None)
+
+    # 테이블 CREATE 및 INSERT
+    code_data.to_sql('CODE_DATA', conn)
+
+    return
+
+# 초기 적재 수행
+#index_load_initial()
+
+#일별 수행
+#index_load_daily()
+
+stocks_load_all()
