707e766d7b65c410b3af21eb18599bc6aa8925c0
junion <junion.park@gmail.com>
2021-02-16 22:03:48 +0900

    종목코드 read_csv dtype 지정

--- SOURCE/server/CodeName.py
+++ SOURCE/server/CodeName.py
@@ -1,14 +1,15 @@
-
 import pandas as pd
+
+import server_main as sm
 
 
 class SearchCode:
     def __init__(self):
-        self.df_code_name = pd.read_csv('../data/code-name.csv')
+        self.df_code_name = pd.read_csv('../data/code-name.csv', dtype={'code':str})
 
     def getDataCodeName(self, sKeyword):
-        sKeyword = sKeyword.strip()
-        print('>> 검색어 ['+sKeyword+']')
+        sKeyword = sKeyword.strip().upper()
+        print('>> 검색어 [' + sKeyword + ']')
         # print(df_code_name.columns)
 
         list_search = []
@@ -20,7 +21,7 @@ class SearchCode:
                     break
 
         df_search = pd.DataFrame(list_search, columns=self.df_code_name.columns).reset_index()
-        print(df_search[['type','code','name']])
+        print(df_search[['type', 'code', 'name']])
 
         return df_search
 
@@ -28,6 +29,7 @@ class SearchCode:
 if __name__ == '__main__':
     codeName = SearchCode()
     while True:
-        sInput = input('검색어를 입력하세요: ')
+        sInput = input('검색어를 입력하세요(exit=종료): ')
+        if sInput == 'exit': break
         codeName.getDataCodeName(sInput)
         print('--------------------------------------\n')
