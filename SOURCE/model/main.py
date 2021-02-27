1879906ecde79c383a24b5c939dabc7bab65712a
junion <junion.park@gmail.com>
2021-02-27 09:57:39 +0900

    input stock code [static variable] to [list]. if list is empty, request input code once

--- SOURCE/model/main.py
+++ SOURCE/model/main.py
@@ -5,14 +5,10 @@ from datetime import datetime, timedelta
 from tools import *
 from usage import *
 
-file_name = '195870'
-
-
-
-
+# file_name = '316140'
 col = ['Open', 'High', 'Low', 'Close','Volume']
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     '''
     run 학습 함수
     seq_len : 학습시킬 길이 or 테스트 데이터 셋 길이
@@ -22,8 +18,18 @@ if __name__ == "__main__" :
     
     '''
 
-    run(seq_len = 30, number=file_name, learning=True)
-    run(seq_len = 30, number = file_name, learning = False)
+list_file_name = []
 
+if len(list_file_name) == 0:
+    str_stock_code = input('Input Stock Code to Learning : ')
+    if len(str_stock_code) != 6:
+        exit(0)
+    else:
+        list_file_name.append(str_stock_code)
 
+for file_name in list_file_name:
+    print('>>> Learning')
+    run(seq_len=30, number=file_name, learning=True)
 
+    print('>>> Prediction')
+    run(seq_len=30, number=file_name, learning=False)
